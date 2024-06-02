0f83a139e02e059e63090983eec9c5b3f713883c
Xeshan Malik <xeshanmalik107@gmail.com>
2024-06-03 04:37:16 +0500

    collision remaining

--- Space shooter/MainWindow.xaml.cs
+++ Space shooter/MainWindow.xaml.cs
@@ -19,39 +19,106 @@ namespace Space_shooter
     public partial class MainWindow : Window
     {
         DispatcherTimer timer = new DispatcherTimer();
+        List<Rectangle> itemremover = new List<Rectangle>();
         Random rand = new Random();   
         bool moveLeft,moveRight;
-        int leftmovespeed=15,rightmovespeed =15;
+        int leftmovespeed=10,rightmovespeed =10;
         int enemyrandom;
+        int enemyspeed = 5;
+        int enemycounter = 100;
+        int limit = 50;
+        int score;
+        int damage;
+       
         public MainWindow()
         {
             InitializeComponent();
             timer.Interval = TimeSpan.FromMilliseconds(20);
 
             timer.Tick += Playermovement;
+          
             timer.Start();
             mycanvas.Focus();
+            //enemyspawn();
         }
 
         private void Playermovement(object? sender, EventArgs e)
         {
-            if (moveLeft == true && Canvas.GetLeft(player) > 0) 
+            Rect playerhitbox= new Rect(Canvas.GetTop(player),Canvas.GetLeft(player),player.Height,player.Width);
+            enemycounter -= 1;
+            
+            scoretxt.Content = "Score: " + score;
+            damagetxt.Content = "Damage: " + damage;
+            if (enemycounter < 0)
             {
-                //leftmovecount++;
-                Canvas.SetLeft(player,Canvas.GetLeft(player)-leftmovespeed);
+                enemyspawn();
+                enemycounter = limit;
             }
-            if (moveRight == true )
+
+
+            if (moveLeft == true && Canvas.GetLeft(player) > 0)
             {
-                //rightmovecount++;
-                Canvas.SetLeft(player,Canvas.GetLeft(player)+rightmovespeed);
+
+                Canvas.SetLeft(player, Canvas.GetLeft(player) - leftmovespeed);
             }
-        }   
+            if (moveRight == true && Canvas.GetLeft(player) < 340)
+            {
+
+                Canvas.SetLeft(player, Canvas.GetLeft(player) + rightmovespeed);
+            }
+
+            //Bullet Firring//
+            foreach (var a in mycanvas.Children.OfType<Rectangle>())
+            {
+
+                if (a is Rectangle && (string)a.Tag == "bullet")
+                {
+                    Canvas.SetTop(a, Canvas.GetTop(a) - 20);
+                    Rect bullethitbox = new Rect(Canvas.GetLeft(a), Canvas.GetTop(a), a.Height, a.Width);
+                    if (Canvas.GetTop(a) < 10)
+                    {
+                        itemremover.Add(a);
+
+                    }
+                    
+                }
+
+
+
+                if (a is Rectangle && (string)a.Tag == "enemyspawn")
+                {
+                    Canvas.SetTop(a, Canvas.GetTop(a) + enemyspeed);
+
+                    if (Canvas.GetTop(a) > Canvas.GetTop(player)+100)
+                    {
+                        itemremover.Add(a);
+                        damage += 1;
+                    }
+                    Rect enemyhitbox = new Rect(Canvas.GetLeft(a), Canvas.GetTop(a), a.Height, a.Width);
+                    
+
+                    if (playerhitbox.IntersectsWith(enemyhitbox))
+                    {
+                        MessageBox.Show("sex sux");
+                        itemremover.Add(a);
+                        damage += 10;
+                        
+                    }
+                    
+                }
+            }
+            foreach(Rectangle i in itemremover)
+            {
+                mycanvas.Children.Remove(i);
+            }
             
 
+        }
         private void mycanvas_KeyDown(object sender, KeyEventArgs e)
         {
             if (e.Key == Key.Left)
             {
+                
                 moveLeft = true;
               
             }
@@ -99,8 +166,7 @@ namespace Space_shooter
 
                 case 1:
                     enemy.ImageSource = new BitmapImage(new Uri("pack://application:,,,/images/1.png"));
-                    //MessageBox.Show("case1");                    enemy.ImageSource = new BitmapImage(new Uri("pack://application:,,,/images/2.png"));
-
+                                       
                     break;
                     
                 case 2:
@@ -124,13 +190,13 @@ namespace Space_shooter
             Rectangle newenemy = new Rectangle
             {
                 Tag = "enemyspawn",
-                Height = 20,
-                Width = 20,
+                Height = 30,
+                Width = 30,
                 Fill = enemy
 
             };
-            Canvas.SetTop(newenemy, 40);
-            Canvas.SetLeft(newenemy,rand.Next(30,400)); 
+            Canvas.SetTop(newenemy, -100);
+            Canvas.SetLeft(newenemy,rand.Next(20,350)); 
             mycanvas.Children.Add(newenemy);
         }
       
